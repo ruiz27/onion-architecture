1fe096f1cdfaec246199e8fbbbed95e0419a281a
Sergio Ruiz Combariza <>
2022-04-03 20:33:34 +0200

    develop structure

--- src/main/java/com/exercise/onion/application/CreateDomainUseCase.java
+++ src/main/java/com/exercise/onion/application/CreateDomainUseCase.java
@@ -2,7 +2,7 @@ package com.exercise.onion.application;
 
 public class CreateDomainUseCase {
 
-
+    // orchestra the business logic with domain.service
 
 
 }
---
+++ src/main/java/com/exercise/onion/infrastructure/mail/MailManager.java
@@ -0,0 +1,4 @@
+package com.exercise.onion.infrastructure.mail;
+
+public class MailManager {
+}
