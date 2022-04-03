b633fe67161090b536035bd79d64d33397f9fb03
Sergio Ruiz Combariza <>
2022-04-03 19:16:35 +0200

    refact structure

--- src/main/java/com/exercise/onion/OnionApplication.java
+++ src/main/java/com/exercise/onion/OnionApplication.java
@@ -1,4 +1,4 @@
-package com.exercise.onion.application;
+package com.exercise.onion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
