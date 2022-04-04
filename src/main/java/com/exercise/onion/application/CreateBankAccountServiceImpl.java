d7951237a159a7d7f589f398f2c90dd7d1620a5e
sruizvass <sergio.ruiz@vass.es>
2022-04-04 21:13:18 +0200

    updated example

--- src/main/java/com/exercise/onion/application/CreateBankAccountServiceImpl.java
+++ src/main/java/com/exercise/onion/application/CreateBankAccountServiceImpl.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Service;
 public class CreateBankAccountServiceImpl implements CreateBankAccountService{
 
     // orchestra the business logic with domain.service
+    public CreateBankAccountServiceImpl(){
+        // inyecta servicio de usuario para validar si el usuario existe
+        // inyecta servicio de cuentas para crear la cuenta
+    }
 
 
     public String createAccount(){
+       // utilizo el servicio de usuarios y valido que exista
+       // si existe, utilizo servicio de cuenta, para crear la cuenta
+       // retorno el resultado
         return null;
+
     }
 }
